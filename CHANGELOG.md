5c3dba996b32defe78b91177745d80c0f3a7d7b1
Pierre <brol31@gmail.com>
2023-01-25 03:42:57 +0100

    1.4-dev

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+1.4-dev - 25/01/203 - Pierre Van Glabeke
+* dc 2.24 requis
+* modifs html (jeu mustek)
+* modifs css
---
+++ locales/fr/public.lang.php
@@ -0,0 +1,30 @@
+<?php
+// Language: Français
+// Module: dc2smartphone - 
+// Date: 2023-01-25 01:27:24
+// Author: Pierre Van Glabeke
+// Translated with dcTranslater - 2022.12.26
+
+
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Theme by'] = 'Thème par';
+l10n::$locales['Category:'] = 'Catégorie :';
+l10n::$locales['in'] = 'dans';
+l10n::$locales['Informations on the ticket'] = 'Informations sur le billet';
+l10n::$locales['of the ticket'] = 'du billet';
+l10n::$locales['To make a'] = 'Faire un';
+l10n::$locales['ping'] = 'rétrolien';
+l10n::$locales['on this ticket'] = 'sur ce billet';
+l10n::$locales['Your comment'] = 'Votre commentaire';
+l10n::$locales['Browse the category'] = 'Naviguez dans la catégorie';
+l10n::$locales['Tags:'] = 'Mots-clés :';
+l10n::$locales['You liked this ticket? You can subscribe to'] = 'Vous avez aimé ce billet ? Vous pouvez vous abonner au';
+l10n::$locales['entries feed of this blog'] = 'fil des billets de ce blog';
+l10n::$locales['thus you will be automatically informed of all new ticket!'] = 'ainsi vous serez automatiquement averti de tout nouveau billet !';
+l10n::$locales['You do not know what is a flow RSS.'] = 'Vous ne savez pas ce qu\'est un flux RSS.';
+l10n::$locales['View a video explanatory'] = 'Visionnez une vidéo explicative';
+l10n::$locales['French video explanatory of the interest of flows RSS'] = 'Vidéo explicative en français de l\'intérêt des flux RSS';
+l10n::$locales['Category'] = 'Catégorie';
